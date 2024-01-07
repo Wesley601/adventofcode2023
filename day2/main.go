a4cc8a2fdf76e875648db945364fba7f6d75cac6
wesley601 <wesleypassos601@gmail.com>
2024-01-07 18:15:05 -0300

    finish day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 const (
-	RED   = 12
-	GREEN = 13
-	BLUE  = 14
+	MAX_RED   = 12
+	MAX_GREEN = 13
+	MAX_BLUE  = 14
 )
 
 func main() {
@@ -23,14 +25,115 @@ func main() {
 
 	scanner.Split(bufio.ScanLines)
 
+	solution := Part2(scanner)
+
+	fmt.Printf("solution: %v\n", solution)
+}
+
+func Part1(scanner *bufio.Scanner) int {
+	possibleGameSum := 0
+
+outter:
 	for scanner.Scan() {
-		var gameID string
 		text := scanner.Text()
+		games, gameID := SplitGame(text)
 
-		values := strings.Split(text, ":")
+		gameIDInt, err := strconv.Atoi(gameID)
+		if err != nil {
+			panic(err)
+		}
 
-		gameID = strings.Split(values[0], " ")[1]
+		for _, g := range games {
+			if !IsPossibleGame(g) {
+				continue outter
+			}
+		}
 
-		strings.Split(values[1], ", ")
+		possibleGameSum += gameIDInt
 	}
+
+	return possibleGameSum
+}
+
+func Part2(scanner *bufio.Scanner) int {
+	solution := 0
+
+	for scanner.Scan() {
+		max_red := 1
+		max_green := 1
+		max_blue := 1
+		text := scanner.Text()
+		games, _ := SplitGame(text)
+
+		for _, vv := range games {
+			for _, v := range strings.Split(vv, ",") {
+				color, valueInt, err := GetColorAndValue(v)
+				if err != nil {
+					panic(err)
+				}
+
+				switch color {
+				case "red":
+					if valueInt > max_red {
+						max_red = valueInt
+					}
+				case "green":
+					if valueInt > max_green {
+						max_green = valueInt
+					}
+				case "blue":
+					if valueInt > max_blue {
+						max_blue = valueInt
+					}
+				}
+			}
+		}
+
+		solution += max_red * max_green * max_blue
+	}
+
+	return solution
+}
+
+func SplitGame(text string) ([]string, string) {
+	games := strings.Split(text, ";")
+	firstPos := games[0]
+
+	tmp := strings.Split(firstPos, ":")
+
+	gameID := strings.Split(tmp[0], " ")[1]
+	games[0] = tmp[1]
+
+	return games, gameID
+}
+
+func IsPossibleGame(s string) bool {
+	for _, v := range strings.Split(s, ",") {
+		color, valueInt, err := GetColorAndValue(v)
+		if err != nil {
+			panic(err)
+		}
+
+		if color == "red" && valueInt > MAX_RED {
+			return false
+		}
+
+		if color == "green" && valueInt > MAX_GREEN {
+			return false
+		}
+		if color == "blue" && valueInt > MAX_BLUE {
+			return false
+		}
+	}
+
+	return true
+}
+
+func GetColorAndValue(v string) (string, int, error) {
+	tmp := strings.Split(strings.TrimPrefix(v, " "), " ")
+	color := tmp[1]
+	value := tmp[0]
+
+	valueInt, err := strconv.Atoi(value)
+	return color, valueInt, err
 }
